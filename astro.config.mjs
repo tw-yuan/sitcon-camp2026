236c35cec0ab8b26c2d7a79ff861884fe937ce2e
Yorukot <hi@yorukot.me>
2026-04-12 20:45:39 +0800

    feat: add github pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sitcon.camp',
+  base: '/2026',
   vite: {
     plugins: [tailwindcss()]
   }
