c25a7da0fa8b6e7c192f9e9178e0ff672273f398
Yorukot <hi@yorukot.me>
2026-04-12 22:25:38 +0800

    feat: add SEO add video at hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import favicons from 'astro-favicons';
 
+import partytown from '@astrojs/partytown';
+
 const base = '/2026';
 
 // https://astro.build/config
@@ -18,12 +20,15 @@ export default defineConfig({
 
   integrations: [
     favicons({
+      name: 'SITCON Camp 2026',
+      short_name: 'SITCON Camp',
       output: {
         images: true,
         files: true,
         html: true,
         assetsPrefix: base
       }
-    })
+    }),
+    partytown()
   ]
 });
